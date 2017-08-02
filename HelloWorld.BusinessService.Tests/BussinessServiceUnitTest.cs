66e93f199ae1c95954bc1b554b3dadcd2da848f9
sapnasaini <30600278+sapnasaini@users.noreply.github.com>
2017-08-02 12:49:58 -0400

    Add files via upload

---
+++ HelloWorld.BusinessService.Tests/BussinessServiceUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Http;
+using HelloWorld.BusinessService.Controllers;
+
+namespace HelloWorld.BusinessService.Tests
+{
+    /// <summary>
+    ///     Unit tests for  Hello World Business service    /// </summary>
+    [TestClass]
+    public class HelloWorldUnitTest
+    {
+        [TestMethod]
+        public void TestHelloWorldMessage()
+        {
+            string expectedresult= "Hello World";
+            HelloWorldController objHelloworlcontroller=new HelloWorldController();
+            Assert.AreEqual(objHelloworlcontroller.Get(),
+                       expectedresult);
+        }
+
+
+    }
+  
+}
