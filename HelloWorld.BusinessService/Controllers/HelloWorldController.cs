9a36bdc915ebe7e7a9c3d427c4e54d6650e0041b
sapnasaini <30600278+sapnasaini@users.noreply.github.com>
2017-08-02 12:54:00 -0400

    Add files via upload

--- HelloWorld.BusinessService/Controllers/HelloWorldController.cs
+++ HelloWorld.BusinessService/Controllers/HelloWorldController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-
+/// <summary>
+///     API controller for getting string.
+/// </summary>
 namespace HelloWorld.BusinessService.Controllers
 {
     public class HelloWorldController : ApiController
